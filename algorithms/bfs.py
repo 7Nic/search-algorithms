bc88f1ed5b11ff7658cd3c59e2b7b27b1f7d089e
7Nic <gabriel.nicolau97@hotmail.com>
2020-10-06 11:14:05 -0300

    Fixed mazes

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -1,7 +1,19 @@
 
 def main():
-  print('teste')
+  f = open("../mazes/maze1.txt", "r")
+  mazeInfo = f.readline().split()
+  width = int(mazeInfo[0])
+  height = int(mazeInfo[1])
+  maze = [[0 for x in range(width)] for y in range(height)] 
+  
+  for i in range (0, width):
+    fileLine = f.readline()
+    for j in range (0, height):
+      maze[i][j] = fileLine[j]
+
+  print(maze[9][9])
+
+  
 
 
-# Tell python to run main method
 if __name__ == "__main__": main()
